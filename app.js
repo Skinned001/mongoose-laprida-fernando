5bc4f5d93255430034369b1c05ea926e792bf869
Laprida <psmathe001@gmail.com>
2025-09-14 23:38:25 -0300

    database.js configurado

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./src/config/database.js";
+import { userRoutes } from "./src/routes/user.routes.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// rutas
+app.use("/api", userRoutes);
+
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
